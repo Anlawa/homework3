d387122d928cdac99a76a4b76e51dc4791e56ec0
Anlawa <110259133+Anlawa@users.noreply.github.com>
2022-08-05 10:08:42 +0300

    первый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,61 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        //задание 1
+
+        int box = 8;
+        byte bottle = 15;
+        long cat = 90L;
+        short dog = 2;
+        double saltWeight = 7.6;
+        float mouseWeight = 7.5f;
+        boolean boxWeight = box > 10;
+        char bar = 33;
+        System.out.println(bar);
+
+
+
+        // Задание 2
+
+        double boxer1 = 78.2;
+        double boxer2 = 82.7;
+        double weightSum = boxer1 + boxer2;
+        System.out.println("Вес двух боксеров составит" + weightSum + "кг");
+        double weightDifference = boxer1 - boxer2;
+        System.out.println("Разница в весе составит " + weightDifference + "кг");
+
+
+
+
+        // Задание 3
+
+        int bananas = 5;
+        int milk = 200;
+        int iceCream = 2;
+        int egg = 4;
+        int weightProducts = bananas * 80 + milk / 100 * 105 + iceCream * 100 + egg * 70;
+        System.out.println("Вес продуктов составил " + weightProducts + "гр");
+        float inKg = weightProducts / 1000;
+        System.out.println("Вес в килограммах составляет" + inKg + "кг");
+
+        // Задание 4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
